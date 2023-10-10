acd737539187a1d90302ace1d3c5cd2539f83a31
David <102869@isp.cz>
2023-10-11 01:23:33 +0200

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
 display = Display(visible=0, size=(800, 800))  
@@ -33,8 +37,26 @@ for option in options:
     
 driver = webdriver.Chrome(options = chrome_options)
 
-driver.get('http://github.com')
+url = "https://web.toddleapp.com/platform?type=loginForm&usertype=student"
+driver.get(url)
+
+# Find the username and password input fields and submit button by their names
+username_input = driver.find_element_by_name("username")
+password_input = driver.find_element_by_name("password")
+submit_button = driver.find_element_by_name("loginForm")
+
+# Enter the username and password
+username_input.send_keys("username")
+password_input.send_keys("password")
+
+# Submit the form
+submit_button.click()
+
+wait = WebDriverWait(driver, 15)  # Adjust the timeout as needed
+login_button = wait.until(
+    EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/div[1]/div[1]/div[2]/div/div[2]/div/div[2]/div[1]/div[1]/div[2]/button"))
+)
+
 print(driver.title)
 with open('./GitHub_Action_Results.txt', 'w') as f:
     f.write(f"This was written with a GitHub action {driver.title}")
-
