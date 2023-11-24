e5905bedb0678101e7aae23a909de0599d0f58dd
David <102869@isp.cz>
2023-11-24 02:31:03 +0100

    Update manager.py

--- manager.py
+++ manager.py
@@ -7,4 +7,3 @@ env_vars = os.environ
 # For each environment variable
 for name, value in env_vars.items():
     print("GETTING CALENDAR FOR " + name + " PASSWORD IS " + value)
-    subprocess.run(["python", "scraper.py", name, value])
