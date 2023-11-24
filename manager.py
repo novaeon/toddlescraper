603a7e4e93cf6e137afdce086338ae175183767f
David <102869@isp.cz>
2023-11-24 02:02:39 +0100

    Create manager.py

---
+++ manager.py
@@ -0,0 +1,10 @@
+import os
+import subprocess
+
+# Get all environment variables
+env_vars = os.environ
+
+# For each environment variable
+for name, value in env_vars.items():
+    # Call the second Python script with the name and value as arguments
+    subprocess.run(["python", "scraper.py", name, value])
