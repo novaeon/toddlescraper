598a88c509b027da17529fbc7fc15bae73081014
David <102869@isp.cz>
2023-12-01 00:59:35 +0100

    Update manager.py

--- manager.py
+++ manager.py
@@ -16,10 +16,18 @@ def get_all_keys(data, prefix="", keys_list=None):
         keys_list.append(prefix[:-1])  # Add the key without the trailing dot to the list
     return keys_list
 
+def decode_parts(input_string):
+    parts = input_string.split('_')
+    if len(parts) >= 3:
+        decoded_second_part = bytes.fromhex(parts[1]).decode('utf-8')
+        decoded_third_part = bytes.fromhex(parts[2]).decode('utf-8')
+        return decoded_second_part, decoded_third_part
+    else:
+        return None, None
+
 print("PARSED JSON:")
 parsed_data = json.loads(env_vars)
 jsonkeys = get_all_keys(parsed_data)
-print(jsonkeys)
 
 for jsonkey in jsonkeys:
-    print(jsonkey)
+    print(decode_parts(jsonkey))
